ef770f0cb4254b6fc418a45537f8e7f6c70f7248
MakiliGit <maky.stamenic@gmail.com>
2022-05-31 00:06:53 +0200

    Kreiran domenski model

---
+++ app/Models/Item.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    use HasFactory;
+    protected $fillable = ['name', 'price', 'meet', 'item_group_id'];
+    protected $with = ['ingredients'];
+    public function ingredients()
+    {
+        return $this->hasMany(Ingredient::class);
+    }
+
+    public function itemGroup()
+    {
+        return $this->belongsTo(ItemGroup::class);
+    }
+}
